60f2130c8d2dbcf1769923a5765f4bf5e7abf1ae
Jack Laird <jack.laird0@gmail.com>
2020-12-09 13:15:32 -0700

    create file for all api urls

---
+++ src/library/helpers/api-urls.js
@@ -0,0 +1,23 @@
+export const getAllTravelers =
+  "https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers";
+
+export const getSingleTraveler = (id) =>
+  `https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers/${id}`;
+
+export const getAllTrips =
+  "https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips";
+
+export const getAllDestinations =
+  "https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations";
+
+export const addNewTrip =
+  "https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips";
+
+export const addNewDestination =
+  "https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations";
+
+export const modifySingleTrip =
+  "https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/updateTrip";
+
+export const deleteSingleTrip =
+  "https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips";
